f1f3c886658e475462fd8f55305e446aef83ce03
tomoya <shima.apple0941@gmail.com>
2022-12-21 00:11:01 +0900

    use destructuring assignment

--- src/components/ColoredMessage.jsx
+++ src/components/ColoredMessage.jsx
@@ -1,9 +1,14 @@
 export const ColoredMessage = (props) => {
+  // propsを分割代入する
+  const { color, children } = props;
+
   const contentStyle = {
-    color: props.color,
+    // 分割代入をすることにより、props.~の記法じゃなくてよくなる
+    // color: color,
+    color, //省略記法を使用することができる
     fontSize: "40px",
   };
 
-//   return <p style={contentStyle}>{props.message}</p>;
-  return <p style={contentStyle}>{props.children}</p>;
+  //   return <p style={contentStyle}>{props.message}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
